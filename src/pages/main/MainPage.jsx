6ec8f3d9ae51501644cc95a7ebffb5b57da87219
protap1100 <protapb23@gmail.com>
2025-06-06 19:34:41 +0600

    footer added

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
 
 const MainPage = () => {
   return (
     <div className=" container mx-auto">
       <Navbar />
       <Outlet />
+      <Footer></Footer>
     </div>
   );
 };
